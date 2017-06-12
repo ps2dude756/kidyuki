071c5cd39784b3e67885318698e18116b51b057a
Andrew Roth <andrewroth1991@gmail.com>
2017-06-11 18:01:52 -0700

    Add attribution
    Fixes #31

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,13 @@ class Footer extends Component {
     render(): Element {
         return (
                 <div>
-                    <div className="footer">Kid Yuki &copy; 2017</div>
+                    <div className="footer-container">
+                        <div className="footer">
+                            <div>Kid Yuki &copy; 2017</div>
+                            <div className="attribution"><a href="https://github.com/ps2dude756">Site created by Andrew Roth</a></div>
+                            <div className="attribution"><a href="https://www.instagram.com/robo.maru/">Site assets created by Maru</a></div>
+                        </div>
+                    </div>
                 </div>
         );
     }
