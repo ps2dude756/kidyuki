bef9ad76fe2c25916cbc46d952d1a6a6bbdcd3dd
Andrew Roth <andrewroth1991@gmail.com>
2018-04-22 13:00:24 -0700

    Update design to work on desktop

--- src/components/HomepageHeader.js
+++ src/components/HomepageHeader.js
@@ -3,8 +3,10 @@ import React, { Component, Element } from 'react';
 class HomepageHeader extends Component {
     render(): Element {
         return (
-                <div>
-                    <img className="homepage-banner" src="/homepage_banner.jpg"/>
+                <div className="homepage-banner-background">
+                    <div className="homepage-banner-container">
+                        <img className="homepage-banner" src="/homepage_banner.jpg"/>
+                    </div>
                 </div>
         );
     }
